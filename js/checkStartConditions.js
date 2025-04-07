d3105b436d9cf452379c41b505e170c41079fe7b
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-07 17:11:25 +0500

    new

--- js/checkStartConditions.js
+++ js/checkStartConditions.js
@@ -3,12 +3,12 @@ import { startGame } from './startGame.js';
 
 export function checkStartConditions() {
     const canStart = state.teams.length >= 2 && 
-                     state.teams.every(team => team.name.length > 0);
+    state.teams.every(team => team.name.length > 0);
     if (!canStart) {
-        elements.startGame.classList.add("disabled");
-        elements.startGame.removeEventListener("click", startGame);
+    elements.startGame.classList.add("disabled");
+    elements.startGame.removeEventListener("click", startGame);
     } else {
-        elements.startGame.classList.remove("disabled");
-        elements.startGame.addEventListener('click', startGame);
+    elements.startGame.classList.remove("disabled");
+    elements.startGame.addEventListener('click', startGame);
     }
 }
--- js/updateTeamsNumbering.js
+++ js/updateTeamsNumbering.js
@@ -4,5 +4,7 @@ export function updateTeamsNumbering() {
     const inputs = elements.teamsContainer.querySelectorAll('.team-input');
     inputs.forEach((input, index) => {
         input.querySelector('label').textContent = `Команда ${index + 1}`;
+        input.querySelector('div').querySelector(`input`).setAttribute('id', `team-${index}`)
+        input.querySelector('div').querySelector(`div`).setAttribute('id', `remove-${index}`)
     });
 }
